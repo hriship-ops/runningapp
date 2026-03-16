17081daa26022bf48c75911ffd71cd54a7d37ee6
hrishi <hriship@gmail.com>
2026-03-16 08:54:23 +0000

    add get_all_runs method to run.py

--- runningapp/running_journal/doctype/run/run.py
+++ runningapp/running_journal/doctype/run/run.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2026, hrishi and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe, json
 from frappe.model.document import Document
 
-
 class Run(Document):
-	pass
+    pass
+
+@frappe.whitelist()
+def get_all_runs(filters=None):
+    if filters and isinstance(filters, str):
+        filters = json.loads(filters)
+    return frappe.db.get_all(
+        "Run",
+        filters=filters or [],
+        fields=["name","run_name","date","activity_type","location","distance_km","duration_sec","elevation_gain","calories"],
+        order_by="date desc",
+        ignore_permissions=True
+    )
